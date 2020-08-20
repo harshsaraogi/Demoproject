9b42ec83584cf2a668d96d47d053f769771b4f27
Harshvardhan Saraogi <harshvardhansar@youforce.net>
2020-08-20 18:47:18 +0530

    Add project files.

---
+++ Demoproject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demoproject
+{
+          class Program
+          {
+                    static void Main(string[] args)
+                    {
+                              Console.WriteLine("Hello World!");
+                    }
+          }
+}
